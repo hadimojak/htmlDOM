d4407e12f11a26896a45807f5262f611e563cd97
Hadi Arbabi <h.arbabi@banicm.com>
2020-04-11 16:56:51 +0430

    trevarsinDOM

--- app.js
+++ app.js
@@ -15,3 +15,19 @@ const listItemElements = document.getElementsByTagName('li');
 for (const listItemEl of listItemElements) {
   console.dir(listItemEl);
 }
+
+const section = document.querySelector('section');
+const button = document.querySelector('button');
+// section.style.backgroundColor = 'blue';
+section.className = 'red-bg';
+
+button.addEventListener('click', () => {
+  // if (section.className === 'red-bg visible') {
+  //   section.className = 'red-bg invisible';
+  // } else {
+  //   section.className = 'red-bg visible';
+  // }
+
+  section.classList.toggle('visible');
+  section.classList.toggle('invisible');
+});
